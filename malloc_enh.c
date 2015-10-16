ceb414992066c39fb8301588cc3f5e0182befd4d
EthanBerroa <ethan.berroa@gmail.com>
2015-10-16 01:33:32 -0400

    Update malloc_enh.c
    Fixed, barely.

--- malloc_enh.c
+++ malloc_enh.c
@@ -1,8 +1,9 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define malloc( x )	myMalloc( x, __FILE__, __LINE__)
-#define free( x )	myFree(x, __FILE__, __LINE__)
+#define malloc(x)	myMalloc( x, __FILE__, __LINE__)
+#define free(x)		myFree(x, __FILE__, __LINE__)
 
 # define blocksize 1<<20
 # define fudge_factor 5
@@ -14,22 +15,24 @@ typedef struct mementry
 	struct mementry *prev, *succ;
 	int isfree;
 	unsigned int size;
+	//int recognize;
 } mementry;
 
 static char big_block[blocksize];
 
 //static int initialized=0;
 
-void *mymalloc(unsigned int size)
+void *myMalloc(unsigned int size, char * file, int line)
 {
 	if(size>((blocksize)-24))
 	{
 		printf("TOO LARGE CHUNK REQUESTED\n");
+		printf("Call specifics: FILE %s LINE %d\n", file, line);
 		return(0);
 	}
 	static int initialized=0;
 	static mementry *root;
-	mementry *p, *succ;
+	mementry *p, *succ, *prev;
 
 	if(!initialized)
 	{
@@ -42,60 +45,95 @@ void *mymalloc(unsigned int size)
 	}
 	
 	p=root;
+	prev=root;
 	mementry*test=root;
-	printf("%d is the address of root \n",root);
-	do
+	//printf("%d is the address of root \n",root);
+	while(p!=NULL)
 	{
 		if(p->size < size)
+		{
+			prev=p;
+			
 			p=p->succ;
+			
+		}
 		else if(!p->isfree)
+		{
+			prev=p;
+			
 			p=p->succ;
-		else if(p->size < (size + sizeof(mementry) + fudge_factor))
+		
+		}
+		else if(p->size <= (size + sizeof(mementry)))
 		{
 			p->isfree=0;
-			return (char*)p + sizeof(mementry);
+			p->prev=prev;
+			//return (char*)p + sizeof(mementry);
+			return p + sizeof(mementry);
 		}
 		else
 		{
-			succ=(mementry*)((char*)p + sizeof(mementry)) + size; // NOT CORRECT??? IN RECITATION PEOPLE SUGGESTED TO PUT size IN PARENTHESIS!
+			printf("%d\n", __LINE__);
+			succ=(mementry *)((char *)p + sizeof(mementry) + size); // NOT CORRECT??? IN RECITATION PEOPLE SUGGESTED TO PUT size IN PARENTHESIS!
 			succ->prev=p;
 			succ->succ=p->succ;
 			if(p->succ!=NULL)
-				p->succ->prev=succ;
+			{
+				p->succ->prev=succ; //This is just p = succ 
+				printf("%d\n", __LINE__);
+			}
 			p->succ=succ;
-			succ->size=p->size - sizeof(mementry) - size;
+			succ->size= p->size - sizeof(mementry) - size;
 			succ->isfree=1;
 			p->size=size;
 			p->isfree=0;
-			return (char*)p + sizeof(mementry);
+
+			p->prev=prev;			
+			printf("%d p itself\n", p+sizeof(mementry));
+			printf("%d prev\n", p->prev);
+			printf("%d root\n", root);
+			printf("%d root isfree\n", root->isfree);
+			//printf("%d\n __LINE__", p->prev->isfree); //thisbreaksus
+
+			//return (char*)(p + sizeof(mementry));
+			return p + sizeof(mementry);
 		}
-	}while(p!=NULL);
+	}
 
 	printf("ERROR: USED ALL MEMORY");	
 
 	return 0;
 }
-void myfree(void * p1)
+void myFree(void * p1, char * file, int line)
 {
 	mementry *ptr, *pred, *succ;
+	printf("%d p1\n", p1);
 
-	ptr = (mementry *)((char*)p1-sizeof(mementry));
-
-	printf("%d is adress of ptr->prev \n", ptr->prev);
+	ptr = (mementry *)p1-1; 
+	
+	printf("%d is adress of ptr \n", ptr);
 	printf("%d\n",__LINE__);
 
-	if((pred=ptr->prev)!=NULL&& pred->isfree)
+	printf("%d is ptr->prev\n", ptr->prev);
+	printf("%d\n",__LINE__);	//stops here
+
+	if(((pred=ptr->prev)!=NULL) && pred->isfree)
 	{
 		printf("%d\n",__LINE__);		
 		pred->size += sizeof(mementry) + ptr->size;
 		pred->succ = ptr->succ;
+		printf("%d\n", __LINE__);
 		if(ptr->succ!=NULL)
+		{
+			printf("%d \n", __LINE__); //
 			ptr->succ->prev = pred;
+		}
+		printf("%d\n", __LINE__);
 	}
 	else
 	{
 		printf("%d\n",__LINE__);
-		ptr->isfree =1;
+		ptr->isfree = 1;
 		pred = ptr;
 	}
 	if((succ=ptr->succ)!=NULL && succ->isfree)
@@ -103,6 +141,7 @@ void myfree(void * p1)
 		printf("%d\n",__LINE__);
 		pred->size += sizeof(mementry) + succ->size;
 		pred->succ = succ->succ;
+		
 		if(succ->succ!=NULL)
 			succ->succ = pred;
 	}
@@ -112,7 +151,10 @@ void myfree(void * p1)
 
 int main()
 {
-	int* arr=(int*)mymalloc(10*sizeof(int));
+	int *arr= (int *)malloc(10*sizeof(int));
+	
+	printf("%d adress of arr\n", arr);
+
 	arr[0]=1;
 	int i = 0;
 	for(i=0; i<10; i++)
@@ -123,17 +165,19 @@ int main()
 	i++;
 	}
 	
-	char *b= (char *)mymalloc(10*sizeof(char));
+	char *b= (char *)malloc(10*sizeof(char));
+	printf("%d b before\n", b);
 	//printf("%d", __LINE__);
-	b="abcdef";
-	printf("%s\n", b);
-	printf("%d size of char \n", sizeof(mementry));	
-	mementry *x =(mementry *)(arr-6);
-	printf("%d is the address of x \n", x);
-	printf("%d is the free of x \n", x->isfree);
-	myfree(b);
-	printf("%d is the isfree of x after freeing it\n", x->isfree);
-	printf("%d\n", __LINE__);
+	//b="abcdef";
+	/*int g = 0;
+	for(g=0; g<10; g++)
+		b[g]='a'
+	*/
+	printf("%s %d\n", b, b);
+	
+	free(b);
+					//I managed to fix it by not adding any values to B. By adding values to B it for some reason changed the address completely.
 	//printf("%s\n", b);	
 	return(0);
 }
+
